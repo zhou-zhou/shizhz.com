8a66c96ea16473844b2c05167f3d22d078e35bab
Shizhz <messi.shizz@gmail.com>
2018-04-06 10:07:57 +0800

    Init project and add Navbar

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+// @import "~bulma/sass/utilities/_all";
+// @import "~bulma/sass/components/navbar.sass";
+@import "~bulma";
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,60 @@
+<template>
+  <nav class="navbar is-transparent is-fixed-top">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="http://shizhz.com">
+        <img src="@/assets/logo.png" alt="Logo">
+    </a>
+    <div class="navbar-burger burger" data-target="navbar-menu">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+
+  <div id="navbar-menu" class="navbar-menu is-active">
+    <div class="navbar-start">
+      <a class="navbar-item" href="/#">
+        Shizhz's Site
+      </a>
+      <hr class="navbar-divider">
+      <a class="navbar-item" href="/#blogs">
+        Blogs
+      </a>
+      <a class="navbar-item" href="/#tips">
+        Tips
+      </a>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="field">
+          <div class="control has-icons-left has-icons-right">
+            <input class="input" type="input" placeholder="Search Anything">
+            <span class="icon is-medium is-right">
+              <i class="fas fa-search"></i>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div class="navbar-item">
+        <div class="field is-grouped">
+          <p class="control">
+            <a class="button is-primary" href="/#write">
+              <span class="icon">
+                <i class="fas fa-edit"></i>
+              </span>
+              <span>New</span>
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
