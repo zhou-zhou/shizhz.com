ff91141e4ced6ae88c332d826667f82aaf0d9fc6
Shizhz <messi.shizz@gmail.com>
2018-04-15 16:54:48 +0800

    Adjust the layout of editor

--- src/pages/Writing.vue
+++ src/pages/Writing.vue
@@ -1,47 +1,51 @@
 <template>
-  <div style="height:90%;">
+  <div class="editor-wrapper">
     <div id="editor"></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Writing',
-  mounted () {
-    const Quill = require('quill')
+  name: "Writing",
+  mounted() {
+    const Quill = require("quill");
     var toolbarOptions = [
       [{ header: [1, 2, 3, 4, 5, 6, false] }],
       [{ font: [] }],
-      [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
-      ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-      ['blockquote', 'code-block'],
+      [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+      ["bold", "italic", "underline", "strike"], // toggled buttons
+      ["blockquote", "code-block"],
 
-      [{ header: 1 }, { header: 2 }], // custom button values
-      [{ list: 'ordered' }, { list: 'bullet' }],
-      [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
-      [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
-      [{ direction: 'rtl' }], // text direction
+      // [{ header: 1 }, { header: 2 }], // custom button values
+      [{ list: "ordered" }, { list: "bullet" }],
+      [{ script: "sub" }, { script: "super" }], // superscript/subscript
+      [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+      [{ direction: "rtl" }], // text direction
 
       [{ color: [] }, { background: [] }], // dropdown with defaults from theme
       [{ align: [] }],
 
-      ['clean'] // remove formatting button
-    ]
+      ["clean"] // remove formatting button
+    ];
 
-    var options = {
+    new Quill(this.$el.querySelector("#editor"), {
       modules: {
         toolbar: toolbarOptions
       },
-      placeholder: 'Write your article...',
+      placeholder: "Write your article...",
       readOnly: false,
-      theme: 'snow'
-    }
-    new Quill(this.$el.querySelector("#editor"), options)
+      theme: "snow"
+    });
   }
-}
+};
 </script>
 
 <style scoped>
+.editor-wrapper {
+  height: 95%;
+  width: 95%;
+  padding-left: 5%;
+}
 #editor {
   height: 90% !important;
 }
